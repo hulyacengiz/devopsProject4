63e7c5f4e1795fb21e1a9dab2b66f4ac295c6d33
hulyacengiz <hulya.cengiz@ogr.sakarya.edu.tr>
2026-05-21 09:32:55 +0300

    test sunum

--- src/main/java/devops/devopsproject4/HelloController.java
+++ src/main/java/devops/devopsproject4/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "Hello from Kubernetes test5";
+        return "Hello from Kubernetes SUNUM";
     }
 }
 
